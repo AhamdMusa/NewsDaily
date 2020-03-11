cd93dae1f11ff7f21c007391600aad7ef138f172
AhmadMusa <ahmad0musa1@gmail.com>
2020-03-11 16:17:47 +0600

    ToDo, Make Adaptar(%50 done)

--- app/src/main/java/com/brainybrains/newsdaily/adapter/NewsAdapter.java
+++ app/src/main/java/com/brainybrains/newsdaily/adapter/NewsAdapter.java
@@ -4,11 +4,15 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.brainybrains.newsdaily.R;
+import com.brainybrains.newsdaily.fromAPI.News;
+import com.squareup.picasso.Picasso;
 
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
 
@@ -31,8 +35,30 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView miniImage, bigImage,share;
+        private TextView miniTitel, bigTitel, date, bigNews;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            miniImage=itemView.findViewById(R.id.miniCardImage);
+            bigImage=itemView.findViewById(R.id.bigImage);
+            share=itemView.findViewById(R.id.shear);
+            miniTitel=itemView.findViewById(R.id.miniCardText);
+            bigTitel=itemView.findViewById(R.id.bigTitle);
+            date=itemView.findViewById(R.id.bigDate);
+            bigNews=itemView.findViewById(R.id.bigNews);
+        }
+        public void bind(News news) {
+
+            String photoURL=News;
+            Picasso.get()
+                    .load(photoURL)
+                    .error(R.drawable.moon)
+                    .into(placeImageView);
+
+
+            ratingBar.setRating(Float.parseFloat(favorite.getRating()));
+
         }
     }
 }
